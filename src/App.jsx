b14306281159ed6ed00d3c4e0d8587f09381cdc0
Azman <idrisiazman@gmail.com>
2025-01-01 01:09:02 +0530

    About section done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 
+import About from './screens/About'
 import Hero from './screens/Hero'
 const App = () => {
   return (
     <main className='relative min-h-screen w-screen overflow-x-hidden'>
      <Hero />
-     <section 
-     className='z-0 min-h-screen bg-blue-500'
-     />
+    <About />
     </main>
   )
 }
