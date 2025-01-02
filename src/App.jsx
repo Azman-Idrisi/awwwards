daa6d3a2b60358ed3c8a3391ae52c45e9d6aedb5
Azman <idrisiazman@gmail.com>
2025-01-02 21:51:32 +0530

    About and Word Animations of title done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-
+import Navbar from './Navbar'
 import About from './screens/About'
 import Hero from './screens/Hero'
 const App = () => {
   return (
     <main className='relative min-h-screen w-screen overflow-x-hidden'>
+    <Navbar />
      <Hero />
     <About />
     </main>
