e5049e5341cde3451511836a44fa44a63d3caf10
Azman <idrisiazman@gmail.com>
2025-01-03 00:19:47 +0530

    Navbar Done

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import About from './screens/About'
 import Hero from './screens/Hero'
 const App = () => {
   return (
-    <main className='relative min-h-screen w-screen overflow-x-hidden'>
+    <main className='relative min-h-screen w-screen overflow-x-hidden '>
     <Navbar />
      <Hero />
     <About />
