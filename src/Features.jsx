333483b30527f4a46ec777ffeb0a487cdc0c98d1
Azman <idrisiazman@gmail.com>
2025-01-04 00:38:26 +0530

    Bentotilt

--- src/Features.jsx
+++ src/Features.jsx
@@ -1,6 +1,19 @@
 import React from "react";
 import BentoCard from "./components/BentoCard";
 import { TiLocation } from "react-icons/ti";
+import { useState } from "react";
+
+const BentoTilt = ({children , className = ''})=>{
+  
+  const [transfomrStyle , setTransformStyle] = useState('')
+  
+    return(
+        <div className={className}>
+            {children}
+        </div>
+    )
+}
+
 
 const Features = () => {
   return (
@@ -16,7 +29,7 @@ const Features = () => {
             experience on your world.
           </p>
         </div>
-        <div className="border-hsla relative mb-7 h-96 w-full overflow-hidden rounded-md md:h-[65vh]">
+        <BentoTilt className="border-hsla relative mb-7 h-96 w-full overflow-hidden rounded-md md:h-[65vh]">
           <BentoCard
             src="videos/feature-1.mp4"
             title={
@@ -26,9 +39,9 @@ const Features = () => {
             }
             description="A cross-platform metagame app, turning your activities across Web2 and Web3 games into a rewarding adventure."
           />
-        </div>
+        </BentoTilt>
         <div className="grid h-[135vh] grid-cols-2 grid-rows-3 gap-7">
-          <div className="bento-tilt_1 row-span md:col-span-1 md:row-span-2">
+          <BentoTilt className="bento-tilt_1 row-span md:col-span-1 md:row-span-2">
             <BentoCard
               src="videos/feature-2.mp4"
               title={
@@ -38,8 +51,8 @@ const Features = () => {
               }
               description="An Anime and gaming-inspired NFT collection - the IP primed for expansion"
             />
-          </div>
-          <div className="bento-tilt_1 row-span-1 ms-32 md:col-span-1 md:ms-0">
+          </BentoTilt>
+          <BentoTilt className="bento-tilt_1 row-span-1 ms-32 md:col-span-1 md:ms-0">
             <BentoCard
               src="videos/feature-3.mp4"
               title={
@@ -49,8 +62,8 @@ const Features = () => {
               }
               description="A gamified social hub, adding a new dimension of play to social interaction for Web3 communities"
             />
-          </div>
-          <div className="bento-tilt_1 me-14 md:col-span-1 md:me-0">
+          </BentoTilt>
+          <BentoTilt className="bento-tilt_1 me-14 md:col-span-1 md:me-0">
             <BentoCard
               src="videos/feature-4.mp4"
               title={
@@ -60,16 +73,16 @@ const Features = () => {
               }
               description="A cross-world AI Agent - elevating your gameplay to be more fun and productive"
             />
-          </div>
-          <div className="bento-tilt_2">
+          </BentoTilt>
+          <BentoTilt className="bento-tilt_2">
             <div className="flex size-full flex-col justify-between bg-violet-300 p-5">
               <h1 className="bento-title special-font max-w-64 tex-black">
                 M<b>o</b>re co<b>m</b>ing s<b>o</b>on.
               </h1>
               <TiLocation className="m-5 scale-[5] self-end" />
             </div>
-          </div>
-          <div className="bento-tilt_2">
+          </BentoTilt>
+          <BentoTilt className="bento-tilt_2">
             <video 
             src="/videos/feature-5.mp4"
             loop
@@ -77,7 +90,7 @@ const Features = () => {
             autoPlay
             className="size-full object-center object-cover"
             />
-          </div>
+          </BentoTilt>
         </div>
       </div>
     </section>
