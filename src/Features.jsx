21ff685e0b10e8fa80c05f7eada15ad21aa77c97
Azman <idrisiazman@gmail.com>
2025-01-04 00:31:17 +0530

    Features

--- src/Features.jsx
+++ src/Features.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import BentoCard from "./components/BentoCard";
+import { TiLocation } from "react-icons/ti";
 
 const Features = () => {
   return (
@@ -47,6 +48,34 @@ const Features = () => {
                 </>
               }
               description="A gamified social hub, adding a new dimension of play to social interaction for Web3 communities"
+            />
+          </div>
+          <div className="bento-tilt_1 me-14 md:col-span-1 md:me-0">
+            <BentoCard
+              src="videos/feature-4.mp4"
+              title={
+                <>
+                  AZ<b>U</b>LE
+                </>
+              }
+              description="A cross-world AI Agent - elevating your gameplay to be more fun and productive"
+            />
+          </div>
+          <div className="bento-tilt_2">
+            <div className="flex size-full flex-col justify-between bg-violet-300 p-5">
+              <h1 className="bento-title special-font max-w-64 tex-black">
+                M<b>o</b>re co<b>m</b>ing s<b>o</b>on.
+              </h1>
+              <TiLocation className="m-5 scale-[5] self-end" />
+            </div>
+          </div>
+          <div className="bento-tilt_2">
+            <video 
+            src="/videos/feature-5.mp4"
+            loop
+            muted
+            autoPlay
+            className="size-full object-center object-cover"
             />
           </div>
         </div>
